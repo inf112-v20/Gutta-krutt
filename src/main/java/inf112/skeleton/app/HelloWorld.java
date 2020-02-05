cbe993852567b7b5a635fffe15c53d98d6123057
sedrickvarnes <sedrickvarnes@gmail.com>
2020-02-05 13:37:08 +0100

    gdx tutorial nesten ferdig

--- src/main/java/inf112/skeleton/app/HelloWorld.java
+++ src/main/java/inf112/skeleton/app/HelloWorld.java
@@ -4,10 +4,23 @@ import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL30;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
 public class HelloWorld implements ApplicationListener {
+    private  OrthogonalTiledMapRenderer renderer;
+    private TiledMap tiledmap;
+    private TiledMapTileLayer Hole;
+    //private TiledMapTileLayer Player;
+    private TiledMapTileLayer Board;
+    private TiledMapTileLayer Flag;
+
+
     private SpriteBatch batch;
     private BitmapFont font;
 
@@ -16,6 +29,26 @@ public class HelloWorld implements ApplicationListener {
         batch = new SpriteBatch();
         font = new BitmapFont();
         font.setColor(Color.RED);
+
+        //initialize a new tilemap
+        TmxMapLoader tmxLoader = new TmxMapLoader();
+        tiledmap = tmxLoader.load("../../../assets/testBoard.tmx");
+
+        //initialize a new camera and renderer for camera
+        OrthographicCamera camera = new OrthographicCamera();
+        renderer = new OrthogonalTiledMapRenderer(tiledmap, 1/300);
+
+        camera.setToOrtho(false, (float) 2.5 ,5);
+        camera.update();
+        renderer.setView(camera);
+
+
+
+        Board = (TiledMapTileLayer) tiledmap.getLayers().get("Board");
+        Flag = (TiledMapTileLayer) tiledmap.getLayers().get("Flag");
+        Hole = (TiledMapTileLayer) tiledmap.getLayers().get("Hole");
+
+
     }
 
     @Override
@@ -29,9 +62,7 @@ public class HelloWorld implements ApplicationListener {
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT);
 
-        batch.begin();
-        font.draw(batch, "Hello World", 200, 200);
-        batch.end();
+        renderer.render();
     }
 
     @Override
