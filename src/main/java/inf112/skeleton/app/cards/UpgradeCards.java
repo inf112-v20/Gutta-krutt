e1a9d0fa7b6ab169fc53008461f3ff3753abdd05
oma015 <oskar.marthinussen@student.uib.no>
2020-02-24 18:00:11 +0100

    Begynt å utvikle upgrade kort.

--- src/main/java/inf112/skeleton/app/cards/UpgradeCards.java
+++ src/main/java/inf112/skeleton/app/cards/UpgradeCards.java
@@ -1,4 +1,20 @@
 package inf112.skeleton.app.cards;
 
-public class UpgradeCards {
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class UpgradeCards extends Cards {
+    static ArrayList<String[]> upgrade = new ArrayList<String[]>();
+
+    static{
+        String[] dualProc = {"Dual Processor", "Description"};
+        String[] tractorBeam = {"Tractor Beam", "Description"};
+        String[] conditionalProgram = {"Conditional Program", "Description"};
+        String[] miniHowitzer = {"Mini Howitzer", "Description"};
+
+        upgrade.add(dualProc);
+        upgrade.add(tractorBeam);
+        upgrade.add(conditionalProgram);
+        upgrade.add(miniHowitzer);
+    }
 }
