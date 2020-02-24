8c3b3fbc0fe460e81759de4719888f768dd10faf
Fredrik Nestvold Larsen <fredrik9700@gmail.com>
2020-02-24 15:29:02 +0100

    In progress: Arbeidsoppgaver og dealer klasse

---
+++ src/main/java/inf112/skeleton/app/game/Dealer.java
@@ -0,0 +1,27 @@
+package inf112.skeleton.app.game;
+import inf112.skeleton.app.cards.CardStack;
+
+public class Dealer {
+
+    int numberOfPlayers; // Number of players
+    int numberOfCards; // Number of cards to be dealt
+
+
+    public Dealer(int players) {
+        this.numberOfPlayers = players;
+    }
+
+    public void draw(int cards) {
+
+    }
+
+    public void dealer() {
+        for(int p = 0; p < numberOfPlayers; p++){
+            for (int c = 0; c < numberOfCards; c++){
+
+            }
+        }
+
+    }
+
+}
