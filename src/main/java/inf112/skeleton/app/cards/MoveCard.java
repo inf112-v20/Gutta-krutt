ee9f550b9c12469140e4f89b2d5d2a7acee6f02d
sedrickvarnes <sedrickvarnes@gmail.com>
2020-02-26 14:59:47 +0100

    "added constructor"

--- src/main/java/inf112/skeleton/app/cards/MoveCard.java
+++ src/main/java/inf112/skeleton/app/cards/MoveCard.java
@@ -2,4 +2,7 @@ package inf112.skeleton.app.cards;
 
 public class MoveCard extends Card {
 
+    public MoveCard(int distance, int priority, Direction direction) {
+        super(distance, priority, direction);
+    }
 }
