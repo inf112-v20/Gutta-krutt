988b85db1f9e8dc8bfa7d15f2e7d3955b1eb0105
oma015 <oskar.marthinussen@student.uib.no>
2020-03-06 10:59:31 +0100

    fixed player array

--- src/main/java/inf112/skeleton/app/Game.java
+++ src/main/java/inf112/skeleton/app/Game.java
@@ -3,17 +3,11 @@ package inf112.skeleton.app;
 import com.badlogic.gdx.*;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL30;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
-import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
-import com.badlogic.gdx.maps.tiled.tiles.StaticTiledMapTile;
-import com.badlogic.gdx.math.Vector2;
 import inf112.skeleton.app.Movement.MovementHandler;
 import inf112.skeleton.app.Player.Player;
 import inf112.skeleton.app.screen.GameScreen;
@@ -38,12 +32,10 @@ public class Game extends InputAdapter implements ApplicationListener  {
         //initialize a new tilemap
         TmxMapLoader tmxLoader = new TmxMapLoader();
         tilemap = tmxLoader.load("assets/map1.tmx");
-
         //initialize all players
         createPlayers();
 
         gameScreen = new GameScreen(tilemap);
-
         movementHandler = new MovementHandler(playerList[0], tilemap);
         Gdx.input.setInputProcessor(this);
     }
