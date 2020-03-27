912e20008486bc93265f4f499e52b238e703f3e3
Fredrik Nestvold Larsen <fredrik9700@gmail.com>
2020-03-27 13:17:44 +0100

    workspace: fredriks branch merged

--- src/main/java/inf112/skeleton/app/Movement/CaseHandler.java
+++ src/main/java/inf112/skeleton/app/Movement/CaseHandler.java
@@ -75,6 +75,8 @@ public class CaseHandler {
         }
 
         switch (allSequences) {
+
+
             //Repair and upgrade
             case TILE_WRENCH:
                 player.repairRobot(1);
