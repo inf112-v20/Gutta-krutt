80e802508dc920c4589383c914a607bf454ac3e5
Fredrik Nestvold Larsen <fredrik9700@gmail.com>
2020-03-27 13:18:25 +0100

    workspace: fredriks branch merged

--- src/main/java/inf112/skeleton/app/board/Board.java
+++
@@ -1,4 +0,0 @@
-package inf112.skeleton.app.board;
-
-public class Board {
-}
--- src/main/java/inf112/skeleton/app/board/IBoard.java
+++
@@ -1,8 +0,0 @@
-package inf112.skeleton.app.board;
-
-public interface    IBoard {
-
-    public void gameSounds();
-
-
-}
