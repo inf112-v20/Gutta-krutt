e88f31aeedecae148e43e188e856d11ae795e9d5
sedrickvarnes <sedrickvarnes@gmail.com>
2020-04-01 14:51:01 +0200

    refactoring

--- src/main/java/inf112/skeleton/app/cards/Direction.java
+++ src/main/java/inf112/skeleton/app/cards/Direction.java
@@ -7,26 +7,7 @@ package inf112.skeleton.app.cards;
 
 public enum Direction {
     NORTH, EAST, SOUTH, WEST, DEFAULT;
-
-    /**
-     * @param dir is the direction player is facing,
-     * @return the the next direction player should be facing.
-     */
-
-
-    public static Direction getDir(Direction dir, Direction east, Direction west) {
-        if (dir.equals(west)){
-            return NORTH;
-        } else if(dir.equals(NORTH)) {
-            return east;
-        } else if (dir.equals(east)) {
-            return SOUTH;
-        } else if (dir.equals(SOUTH)) {
-            return west;
-        } else {
-            throw new IllegalStateException();
-        }
-    }
+    
     //Getting reversed direction of player
     static public Direction invert(Direction dir) {
         switch (dir) {
